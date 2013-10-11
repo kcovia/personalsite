77994edc69b8c84ec4d3cc97f39cd127c4d5ca1d
Kiefer Covia <kcovia@gmail.com>
2013-10-10 21:43:42 -0400

    Added rails app

---
+++ personalsite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personalsite::Application.config.secret_token = '079122344853c538fc7d1306e1a7b0ecc3dc5f2b2e113b00703bb7171f9035f6b126084ef29af89480d61bf62c1f8733600455e37f55a41b279e272b12236529'
